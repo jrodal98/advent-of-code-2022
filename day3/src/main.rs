b5be837ec100c926647e8b0bc3975a4967e0d97c
Jacob Rodal <jrodal@fb.com>
2022-12-02 22:48:05 -0800

    day 3 - refactor
    day 3 - refactor
    
    day 3 - refactor
    
    day 3 - refactor

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,5 @@
+mod prority;
+
 use std::collections::HashSet;
 
 fn main() {
@@ -6,102 +8,35 @@ fn main() {
     println!("Problem 2: {}", problem2(input));
 }
 
-// I could probably do some ascii mathz, but I feel like
-// the compiler could optimize a match statement better?
-fn char_to_priority(c: char) -> u32 {
-    match c {
-        'a' => 1,
-        'b' => 2,
-        'c' => 3,
-        'd' => 4,
-        'e' => 5,
-        'f' => 6,
-        'g' => 7,
-        'h' => 8,
-        'i' => 9,
-        'j' => 10,
-        'k' => 11,
-        'l' => 12,
-        'm' => 13,
-        'n' => 14,
-        'o' => 15,
-        'p' => 16,
-        'q' => 17,
-        'r' => 18,
-        's' => 19,
-        't' => 20,
-        'u' => 21,
-        'v' => 22,
-        'w' => 23,
-        'x' => 24,
-        'y' => 25,
-        'z' => 26,
-        'A' => 27,
-        'B' => 28,
-        'C' => 29,
-        'D' => 30,
-        'E' => 31,
-        'F' => 32,
-        'G' => 33,
-        'H' => 34,
-        'I' => 35,
-        'J' => 36,
-        'K' => 37,
-        'L' => 38,
-        'M' => 39,
-        'N' => 40,
-        'O' => 41,
-        'P' => 42,
-        'Q' => 43,
-        'R' => 44,
-        'S' => 45,
-        'T' => 46,
-        'U' => 47,
-        'V' => 48,
-        'W' => 49,
-        'X' => 50,
-        'Y' => 51,
-        'Z' => 52,
-        _ => unreachable!("Invalid input"),
-    }
-}
-
 fn problem1(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| -> u32 {
-            let rucksack_size = line.len() / 2;
-            let rucksack1: HashSet<char> = line[..rucksack_size].chars().collect();
-            let rucksack2: HashSet<char> = line[rucksack_size..].chars().collect();
+    input.lines().fold(0, |acc, line| {
+        let (sack1_str, sack2) = line.split_at(line.len() / 2);
+        let sack1: HashSet<char> = sack1_str.chars().collect();
 
-            let duplicate = rucksack1.intersection(&rucksack2).collect::<Vec<&char>>()[0];
-            char_to_priority(*duplicate)
-        })
-        .sum()
+        for c in sack2.chars() {
+            if sack1.contains(&c) {
+                return acc + prority::char_to_priority(c);
+            }
+        }
+        unreachable!("Invalid input - no duplicate char")
+    })
 }
 
 fn problem2(input: &str) -> u32 {
-    input
-        .lines()
-        .collect::<Vec<&str>>()
-        .chunks(3)
-        .map(|chunk| {
-            if let &[sack1, sack2, sack3] = chunk {
-                let set1: HashSet<char> = sack1.chars().collect();
-                let set2: HashSet<char> = sack2.chars().collect();
-                let set3: HashSet<char> = sack3.chars().collect();
-
-                for c in set1.intersection(&set2) {
-                    if set3.contains(c) {
-                        return char_to_priority(*c);
-                    }
-                }
-                panic!("Invalid input - no duplicate char")
-            } else {
-                panic!("invalid input - malformed chunks")
+    let mut lines = input.lines().peekable();
+    let mut sum = 0;
+    while lines.peek().is_some() {
+        let set1: HashSet<char> = lines.next().unwrap().chars().collect();
+        let set2: HashSet<char> = lines.next().unwrap().chars().collect();
+        let set3: HashSet<char> = lines.next().unwrap().chars().collect();
+        for c in set1.intersection(&set2) {
+            if set3.contains(c) {
+                sum += prority::char_to_priority(*c);
+                break;
             }
-        })
-        .sum()
+        }
+    }
+    sum
 }
 
 #[test]
