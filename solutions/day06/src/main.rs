064fff8354b637f16ec92063e93ed0cad1a4ed6c
Jacob Rodal <jrodal@fb.com>
2022-12-06 00:05:22 -0800

    day 6 refactor

--- solutions/day06/src/main.rs
+++ solutions/day06/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 fn main() {
     let input = include_str!("../data/input.txt");
     println!("Problem 1: {}", problem1(input));
@@ -10,7 +8,15 @@ fn solve_problem(input: &str, num_distinct_chars: usize) -> usize {
     input
         .as_bytes()
         .windows(num_distinct_chars)
-        .position(|window| window.iter().collect::<HashSet<_>>().len() == num_distinct_chars)
+        .position(|window| {
+            // hyperfine -w 10 -N ./target/release/day06
+            // yields 2.3 ms ± 0.2 ms for below solutions
+            // versus 3.4 ms ± 0.2 using a set to check equality
+            window
+                .iter()
+                .enumerate()
+                .all(|(i, c)| window.iter().skip(i + 1).find(|d| c == *d).is_none())
+        })
         .map(|i| i + num_distinct_chars)
         .unwrap()
 }
