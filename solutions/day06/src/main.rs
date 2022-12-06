5ccbf674570e275317ba9b311195e2e8675275bc
Jacob Rodal <jrodal@fb.com>
2022-12-05 21:48:54 -0800

    day 6

--- solutions/day06/src/main.rs
+++ solutions/day06/src/main.rs
@@ -6,26 +6,25 @@ fn main() {
     println!("Problem 2: {}", problem2(input));
 }
 
-fn solve_problem(input: &str, num_distinct_chars: usize) -> u32 {
-    (input
+fn solve_problem(input: &str, num_distinct_chars: usize) -> usize {
+    input
         .as_bytes()
         .windows(num_distinct_chars)
         .enumerate()
         .filter(|(_, window)| {
-            let s: HashSet<&u8> = HashSet::from_iter(window.iter());
-            s.len() == num_distinct_chars
+            (HashSet::from_iter(window.iter()) as HashSet<&u8>).len() == num_distinct_chars
         })
         .next()
         .unwrap()
         .0
-        + num_distinct_chars) as u32
+        + num_distinct_chars
 }
 
-fn problem1(input: &str) -> u32 {
+fn problem1(input: &str) -> usize {
     solve_problem(input, 4)
 }
 
-fn problem2(input: &str) -> u32 {
+fn problem2(input: &str) -> usize {
     solve_problem(input, 14)
 }
 
