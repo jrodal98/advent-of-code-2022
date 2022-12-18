394de441b16f60a3194f0586553fd3a1a8a0e49b
Jacob Rodal <dev+github@jrodal.com>
2022-12-18 02:46:51 -0500

    day 18

--- solutions/day18/src/main.rs
+++ solutions/day18/src/main.rs
@@ -1,4 +1,4 @@
-use itertools::Itertools;
+use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../data/input.txt");
@@ -6,12 +6,12 @@ fn main() {
     println!("Problem 2: {}", problem2(input));
 }
 
-#[derive(Debug, PartialEq, Eq, sscanf::FromScanf)]
+#[derive(Debug, Hash, PartialEq, Eq, sscanf::FromScanf)]
 #[sscanf(format = "{x},{y},{z}")]
 struct Coordinate {
-    x: usize,
-    y: usize,
-    z: usize,
+    x: isize,
+    y: isize,
+    z: isize,
 }
 
 impl Coordinate {
@@ -28,15 +28,34 @@ impl Coordinate {
                 _ => false,
             }
     }
+
+    fn translate_new(&self, dx: isize, dy: isize, dz: isize) -> Self {
+        Coordinate {
+            x: self.x + dx,
+            y: self.y + dy,
+            z: self.z + dz,
+        }
+    }
+
+    fn adjacent_coordinates(&self) -> Vec<Self> {
+        vec![
+            self.translate_new(1, 0, 0),
+            self.translate_new(-1, 0, 0),
+            self.translate_new(0, 1, 0),
+            self.translate_new(0, -1, 0),
+            self.translate_new(0, 0, 1),
+            self.translate_new(0, 0, -1),
+        ]
+    }
 }
 
 struct Grid {
-    coordinates: Vec<Coordinate>,
+    coordinates: HashSet<Coordinate>,
 }
 
 impl Grid {
     fn new(input: &str) -> Self {
-        let coordinates: Vec<Coordinate> = input
+        let coordinates: HashSet<Coordinate> = input
             .lines()
             .map(|line| sscanf::sscanf!(line, "{Coordinate}").unwrap())
             .collect();
@@ -45,24 +64,58 @@ impl Grid {
 
     fn sa_including_air_pockets(&self) -> usize {
         self.coordinates.len() * 6
-            - 2 * self
+            - self
                 .coordinates
                 .iter()
-                .tuple_combinations()
-                .filter(|(c1, c2)| c1.adjacent(c2))
+                .flat_map(|c| c.adjacent_coordinates())
+                .filter(|c| self.coordinates.contains(c))
                 .count()
     }
 
-    fn is_air_pocket(coordinates: &Vec<&Coordinate>) -> bool {
-        false
-    }
+    fn get_air_pockets(&self) -> HashSet<Coordinate> {
+        let min_x = self.coordinates.iter().map(|c| c.x).min().unwrap_or(0);
+        let max_x = self.coordinates.iter().map(|c| c.x).max().unwrap_or(0);
+        let min_y = self.coordinates.iter().map(|c| c.y).min().unwrap_or(0);
+        let max_y = self.coordinates.iter().map(|c| c.y).max().unwrap_or(0);
+        let min_z = self.coordinates.iter().map(|c| c.z).min().unwrap_or(0);
+        let max_z = self.coordinates.iter().map(|c| c.z).max().unwrap_or(0);
 
-    fn num_air_pockets(&self) -> usize {
-        1
+        let mut pockets: HashSet<Coordinate> = (min_x - 1..=max_x + 1)
+            .flat_map(|x| (min_y - 1..=max_y + 1).map(move |y| (x, y)))
+            .flat_map(|(x, y)| (min_z - 1..=max_z + 1).map(move |z| Coordinate { x, y, z }))
+            .filter(|coord| !self.coordinates.contains(coord))
+            .collect();
+
+        let mut cubes_to_explore = vec![Coordinate {
+            x: min_x - 1,
+            y: min_y - 1,
+            z: min_z - 1,
+        }];
+
+        // Simulate "being the water"
+        while let Some(cube) = cubes_to_explore.pop() {
+            // if the water is able to reach the cube, then it isn't an air pocket
+            if pockets.remove(&cube) {
+                // continue exploring along the space
+                cubes_to_explore.extend(cube.adjacent_coordinates());
+            }
+        }
+
+        pockets
     }
 
     fn sa_excluding_air_pockets(&self) -> usize {
-        self.sa_including_air_pockets() - (self.num_air_pockets() * 6)
+        let including_pockets = self.sa_including_air_pockets();
+        let air_pockets = self.get_air_pockets();
+
+        let air_pocket_penalty = air_pockets.len() * 6
+            - air_pockets
+                .iter()
+                .flat_map(|c| c.adjacent_coordinates())
+                .filter(|c| air_pockets.contains(c))
+                .count();
+
+        including_pockets - air_pocket_penalty
     }
 }
 
